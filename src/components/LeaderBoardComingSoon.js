1d0a7f49d0e57180a4fd5b83c8ff417a6cc1e144
subh05sus <sahasubhadipfkk@gmail.com>
2024-01-29 23:48:57 +0530

    updated favicon and title

---
+++ src/components/LeaderBoardComingSoon.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function LeaderBoardComingSoon() {
+  return (
+    <div className='comingSoon' style={{
+        height:'80vh', 
+        width:'100%',
+        textAlign: 'center',
+        alignItems:'center',
+        display:'flex',
+        justifyContent:'center'
+    }}><div>
+
+        Coming Soon...
+    </div>
+        </div>
+  )
+}
+
+export default LeaderBoardComingSoon
